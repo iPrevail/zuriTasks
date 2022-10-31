bf610b372440d4f14c8159ce23c4695ae20a7d57
iPrevail <102973587+iPrevail@users.noreply.github.com>
2022-10-31 23:47:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/profile", (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).send({
     slackUsername: "iPrevail",
     backend: true,
